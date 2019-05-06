e1b5529521408783d1b25fc7561d13ce6f6fa2b8
mmusteata <marcel.musteata@endava.com>
2019-05-06 12:35:35 +0300

    -- added .gitignore -- added date_time class

---
+++ src/main/java/date_Time_Task/DayOperations.java
@@ -0,0 +1,20 @@
+package date_Time_Task;
+
+public class DayOperations {
+
+//        1.	How many days until Xmas
+//        2.	How much time until Xmas (formatted as: x Months y Days z Hours)
+//        3.	Current time in London/New York/Tokyo
+//        4.	What day of the week was on 18 Oct 1985
+//        5.	Append n chars in a for loop and calculate time consumed by appending.  (milliseconds or nanoseconds) Convert to seconds and format. Compare Builder with + operation.
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
