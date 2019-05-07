b2516850c6add7f8d9eb330a0acfe44aceb6e404
mmusteata <marcel.musteata@endava.com>
2019-05-07 12:14:19 +0300

    added Generic Task

---
+++ src/main/java/generic_task/Coffee.java
@@ -0,0 +1,22 @@
+package generic_task;
+
+import java.util.List;
+
+public class Coffee <T> {
+
+    private List<T> t;
+
+   public Coffee(List<T> t){
+         this.t = t;
+     }
+
+    public List<T> getT() {
+        System.out.println("Waiter serve you with " + t + " coffee");
+        return t;
+    }
+
+    public void setT(List<T> t) {
+        this.t = t;
+    }
+
+}
